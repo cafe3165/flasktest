f7b8b5e80d28b170ae07180815260551f8b616b3
Joseph <78599297@qq.com>
2019-12-20 10:33:34 +0800

    debug

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! yes'
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
